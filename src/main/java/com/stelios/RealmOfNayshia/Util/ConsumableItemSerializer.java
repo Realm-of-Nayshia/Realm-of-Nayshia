11e456a13fe80ca72a54e8193b8ec51250ae3eb0
Stylianos Nektarios Damianakis <snd14641@gmail.com>
2024-05-27 23:22:10 -0400

    feat: improved ConsumableItemSerializer to work exception to a few edge cases

--- src/main/java/com/stelios/RealmOfNayshia/Util/ConsumableItemSerializer.java
+++ src/main/java/com/stelios/RealmOfNayshia/Util/ConsumableItemSerializer.java
@@ -10,98 +10,87 @@ import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.PotionEffect;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
-import org.bukkit.potion.PotionEffect;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class ConsumableItemSerializer implements JsonSerializer<ConsumableItem>, JsonDeserializer<ConsumableItem> {
 
+    private final Gson gson = new GsonBuilder()
+            .registerTypeAdapter(PotionEffect.class, new PotionEffectSerializer())
+            .create();
+
+    private final ItemSerializer itemSerializer = new ItemSerializer();
+
     @Override
     public JsonElement serialize(ConsumableItem consumableItem, Type type, JsonSerializationContext context) {
-        JsonObject jsonObject = new JsonObject();
-
         // Serialize basic item data using the ItemSerializer logic
-        JsonElement itemElement = context.serialize((Item) consumableItem, Item.class);
-        jsonObject = itemElement.getAsJsonObject();
+        JsonObject jsonObject = itemSerializer.serialize((Item) consumableItem, type, context).getAsJsonObject();
 
-        jsonObject.addProperty("foodValue", consumableItem.getItemMeta().getPersistentDataContainer().get(new NamespacedKey(Main.getPlugin(Main.class), "foodValue"), PersistentDataType.INTEGER));
-        jsonObject.addProperty("saturationValue", consumableItem.getItemMeta().getPersistentDataContainer().get(new NamespacedKey(Main.getPlugin(Main.class), "saturationValue"), PersistentDataType.FLOAT));
+        // Get PDC to serialize specific fields
+        PersistentDataContainer pdc = consumableItem.getItemMeta().getPersistentDataContainer();
 
+        // Serialize common PDC fields
+        if (pdc.has(new NamespacedKey(Main.getPlugin(Main.class), "itemType"), PersistentDataType.STRING)) {
+            jsonObject.addProperty("itemType", pdc.get(new NamespacedKey(Main.getPlugin(Main.class), "itemType"), PersistentDataType.STRING));
+        }
+        if (pdc.has(new NamespacedKey(Main.getPlugin(Main.class), "name"), PersistentDataType.STRING)) {
+            jsonObject.addProperty("itemID", pdc.get(new NamespacedKey(Main.getPlugin(Main.class), "name"), PersistentDataType.STRING));
+        }
+        if (pdc.has(new NamespacedKey(Main.getPlugin(Main.class), "unstackable"), PersistentDataType.BOOLEAN)) {
+            jsonObject.addProperty("unstackable", pdc.get(new NamespacedKey(Main.getPlugin(Main.class), "unstackable"), PersistentDataType.BOOLEAN));
+        }
+
+        // Serialize specific fields
+        Integer foodValue = pdc.get(new NamespacedKey(Main.getPlugin(Main.class), "foodValue"), PersistentDataType.INTEGER);
+        if (foodValue != null) {
+            jsonObject.addProperty("foodValue", foodValue);
+        }
+
+        Float saturationValue = pdc.get(new NamespacedKey(Main.getPlugin(Main.class), "saturationValue"), PersistentDataType.FLOAT);
+        if (saturationValue != null) {
+            jsonObject.addProperty("saturationValue", saturationValue);
+        }
+
+        String[] stats = pdc.get(new NamespacedKey(Main.getPlugin(Main.class), "stats"), DataType.STRING_ARRAY);
         JsonArray statsArray = new JsonArray();
-        for (String stat : consumableItem.getStats()) {
-            statsArray.add(stat);
+        if (stats != null) {
+            for (String stat : stats) {
+                statsArray.add(stat);
+            }
         }
         jsonObject.add("stats", statsArray);
 
+        int[] statsAmount = pdc.get(new NamespacedKey(Main.getPlugin(Main.class), "statsAmount"), DataType.INTEGER_ARRAY);
         JsonArray statsAmountArray = new JsonArray();
-        for (int amount : consumableItem.getStatsAmount()) {
-            statsAmountArray.add(amount);
+        if (statsAmount != null) {
+            for (int amount : statsAmount) {
+                statsAmountArray.add(amount);
+            }
         }
         jsonObject.add("statsAmount", statsAmountArray);
 
+        int[] statsDuration = pdc.get(new NamespacedKey(Main.getPlugin(Main.class), "statsDuration"), DataType.INTEGER_ARRAY);
         JsonArray statsDurationArray = new JsonArray();
-        for (int duration : consumableItem.getStatsDuration()) {
-            statsDurationArray.add(duration);
+        if (statsDuration != null) {
+            for (int duration : statsDuration) {
+                statsDurationArray.add(duration);
+            }
         }
         jsonObject.add("statsDuration", statsDurationArray);
 
-        // Serialize PotionEffects
+        PotionEffect[] potionEffects = pdc.get(new NamespacedKey(Main.getPlugin(Main.class), "potionEffects"), DataType.POTION_EFFECT_ARRAY);
         JsonArray potionEffectsArray = new JsonArray();
-        for (PotionEffect potionEffect : consumableItem.getPotionEffects()) {
-            potionEffectsArray.add(context.serialize(potionEffect));
+        if (potionEffects != null) {
+            for (PotionEffect potionEffect : potionEffects) {
+                potionEffectsArray.add(gson.toJsonTree(potionEffect));
+            }
         }
         jsonObject.add("potionEffects", potionEffectsArray);
-
-        // Serialize ItemMeta
-        JsonObject metaObject = new JsonObject();
-        ItemMeta meta = consumableItem.getItemMeta();
-        PersistentDataContainer pdc = meta.getPersistentDataContainer();
-
-        for (NamespacedKey key : pdc.getKeys()) {
-            if (pdc.has(key, PersistentDataType.STRING)) {
-                metaObject.addProperty(key.toString(), pdc.get(key, PersistentDataType.STRING));
-            } else if (pdc.has(key, PersistentDataType.INTEGER)) {
-                metaObject.addProperty(key.toString(), pdc.get(key, PersistentDataType.INTEGER));
-            } else if (pdc.has(key, PersistentDataType.BOOLEAN)) {
-                metaObject.addProperty(key.toString(), pdc.get(key, PersistentDataType.BOOLEAN));
-            } else if (pdc.has(key, PersistentDataType.FLOAT)) {
-                metaObject.addProperty(key.toString(), pdc.get(key, PersistentDataType.FLOAT));
-            } else if (pdc.has(key, DataType.STRING_ARRAY)) {
-                JsonArray array = new JsonArray();
-                for (String value : pdc.get(key, DataType.STRING_ARRAY)) {
-                    array.add(value);
-                }
-                metaObject.add(key.toString(), array);
-            } else if (pdc.has(key, DataType.INTEGER_ARRAY)) {
-                JsonArray array = new JsonArray();
-                for (int value : pdc.get(key, DataType.INTEGER_ARRAY)) {
-                    array.add(value);
-                }
-                metaObject.add(key.toString(), array);
-            }
-        }
-
-        if (meta.hasDisplayName()) {
-            Component displayName = meta.displayName();
-            String displayNameJson = GsonComponentSerializer.gson().serialize(displayName);
-            metaObject.addProperty("displayName", displayNameJson);
-        }
-
-        if (meta.hasLore()) {
-            JsonArray loreArray = new JsonArray();
-            for (Component loreLine : meta.lore()) {
-                String loreLineJson = GsonComponentSerializer.gson().serialize(loreLine);
-                loreArray.add(loreLineJson);
-            }
-            metaObject.add("lore", loreArray);
-        }
-
-        jsonObject.add("itemMeta", metaObject);
 
         return jsonObject;
     }
@@ -110,10 +99,12 @@ public class ConsumableItemSerializer implements JsonSerializer<ConsumableItem>,
     public ConsumableItem deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext context) throws JsonParseException {
         JsonObject jsonObject = jsonElement.getAsJsonObject();
 
-        Material material = Material.valueOf(jsonObject.get("material").getAsString());
-        int amount = jsonObject.get("amount").getAsInt();
-        boolean unstackable = jsonObject.get("unstackable").getAsBoolean();
-        String name = jsonObject.has("name") ? jsonObject.get("name").getAsString() : null;
+        // Deserialize basic item data using the ItemSerializer logic
+        Item item = itemSerializer.deserialize(jsonElement, type, context);
+        Material material = item.getItemStack().getType();
+        int amount = item.getItemStack().getAmount();
+        boolean unstackable = item.isUnstackable();
+        String name = item.getName();
 
         int foodValue = jsonObject.get("foodValue").getAsInt();
         float saturationValue = jsonObject.get("saturationValue").getAsFloat();
@@ -140,7 +131,7 @@ public class ConsumableItemSerializer implements JsonSerializer<ConsumableItem>,
         JsonArray potionEffectsArray = jsonObject.get("potionEffects").getAsJsonArray();
         PotionEffect[] potionEffects = new PotionEffect[potionEffectsArray.size()];
         for (int i = 0; i < potionEffectsArray.size(); i++) {
-            potionEffects[i] = context.deserialize(potionEffectsArray.get(i), PotionEffect.class);
+            potionEffects[i] = gson.fromJson(potionEffectsArray.get(i), PotionEffect.class);
         }
 
         ConsumableItem consumableItem = new ConsumableItem(material, amount, unstackable, name, foodValue, saturationValue, stats, statsAmount, statsDuration, potionEffects);
@@ -149,38 +140,6 @@ public class ConsumableItemSerializer implements JsonSerializer<ConsumableItem>,
         JsonObject metaObject = jsonObject.get("itemMeta").getAsJsonObject();
         ItemMeta meta = consumableItem.getItemStack().getItemMeta();
         PersistentDataContainer pdc = meta.getPersistentDataContainer();
-
-        for (Map.Entry<String, JsonElement> entry : metaObject.entrySet()) {
-            NamespacedKey key = NamespacedKey.fromString(entry.getKey());
-            if (entry.getValue().isJsonPrimitive()) {
-                if (entry.getValue().getAsJsonPrimitive().isString()) {
-                    pdc.set(key, PersistentDataType.STRING, entry.getValue().getAsString());
-                } else if (entry.getValue().getAsJsonPrimitive().isNumber()) {
-                    if (entry.getValue().getAsJsonPrimitive().getAsString().contains(".")) {
-                        pdc.set(key, PersistentDataType.FLOAT, entry.getValue().getAsFloat());
-                    } else {
-                        pdc.set(key, PersistentDataType.INTEGER, entry.getValue().getAsInt());
-                    }
-                } else if (entry.getValue().getAsJsonPrimitive().isBoolean()) {
-                    pdc.set(key, PersistentDataType.BOOLEAN, entry.getValue().getAsBoolean());
-                }
-            } else if (entry.getValue().isJsonArray()) {
-                JsonArray array = entry.getValue().getAsJsonArray();
-                if (array.size() > 0 && array.get(0).isJsonPrimitive() && array.get(0).getAsJsonPrimitive().isString()) {
-                    String[] stringArray = new String[array.size()];
-                    for (int i = 0; i < array.size(); i++) {
-                        stringArray[i] = array.get(i).getAsString();
-                    }
-                    pdc.set(key, DataType.STRING_ARRAY, stringArray);
-                } else if (array.size() > 0 && array.get(0).isJsonPrimitive() && array.get(0).getAsJsonPrimitive().isNumber()) {
-                    int[] intArray = new int[array.size()];
-                    for (int i = 0; i < array.size(); i++) {
-                        intArray[i] = array.get(i).getAsInt();
-                    }
-                    pdc.set(key, DataType.INTEGER_ARRAY, intArray);
-                }
-            }
-        }
 
         if (metaObject.has("displayName")) {
             String displayNameJson = metaObject.get("displayName").getAsString();
@@ -197,6 +156,23 @@ public class ConsumableItemSerializer implements JsonSerializer<ConsumableItem>,
             }
             meta.lore(lore);
         }
+
+        // Set the PDC values
+        if (jsonObject.has("itemType")) {
+            pdc.set(new NamespacedKey(Main.getPlugin(Main.class), "itemType"), PersistentDataType.STRING, jsonObject.get("itemType").getAsString());
+        }
+        if (jsonObject.has("itemID")) {
+            pdc.set(new NamespacedKey(Main.getPlugin(Main.class), "name"), PersistentDataType.STRING, jsonObject.get("itemID").getAsString());
+        }
+        if (jsonObject.has("unstackable")) {
+            pdc.set(new NamespacedKey(Main.getPlugin(Main.class), "unstackable"), PersistentDataType.BOOLEAN, jsonObject.get("unstackable").getAsBoolean());
+        }
+        pdc.set(new NamespacedKey(Main.getPlugin(Main.class), "foodValue"), PersistentDataType.INTEGER, foodValue);
+        pdc.set(new NamespacedKey(Main.getPlugin(Main.class), "saturationValue"), PersistentDataType.FLOAT, saturationValue);
+        pdc.set(new NamespacedKey(Main.getPlugin(Main.class), "stats"), DataType.STRING_ARRAY, stats);
+        pdc.set(new NamespacedKey(Main.getPlugin(Main.class), "statsAmount"), DataType.INTEGER_ARRAY, statsAmount);
+        pdc.set(new NamespacedKey(Main.getPlugin(Main.class), "statsDuration"), DataType.INTEGER_ARRAY, statsDuration);
+        pdc.set(new NamespacedKey(Main.getPlugin(Main.class), "potionEffects"), DataType.POTION_EFFECT_ARRAY, potionEffects);
 
         consumableItem.getItemStack().setItemMeta(meta);
 
